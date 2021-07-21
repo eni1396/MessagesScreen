574eb389e47bc44018504235f297875dc76c03a5
Nikita Entin <entin@vbankcenter.ru>
2021-07-22 02:23:11 +0300

    build mvp module, connected view, presenter and the model

--- MessagesScreen/MessagesScreen/MVP/MessagesViewController.swift
+++ MessagesScreen/MessagesScreen/MVP/MessagesViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class MessagesViewController: UIViewController {
+class MessagesViewController: PresenterDelegate {
 
+    @IBOutlet weak var table: UITableView!
+    
+    private let presenter = MessagesPresenter()
+    private var messages = [Messages]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        presenter.setViewDelegate(delegate: self)
+        presenter.fetchMessages()
     }
 
-
+    func getMessages(messages: [Messages]) {
+        self.messages = messages
+        
+        DispatchQueue.main.async {
+            self.table.reloadData()
+        }
+    }
 }
 
+extension MessagesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "currentChat", for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].user.nickname
+        return cell
+    }
+    
+    
+}
