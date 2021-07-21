656e85fe829263c2270d8d78bfed0275db100c25
Nikita Entin <entin@vbankcenter.ru>
2021-07-22 02:23:11 +0300

    build mvc module, connected view, presenter and the model

--- MessagesScreen/MessagesScreen/MVP/MessagesModel.swift
+++ MessagesScreen/MessagesScreen/MVP/MessagesModel.swift
@@ -8,33 +8,30 @@
 import Foundation
 
 
-struct MessagesModel {
+// MARK: - WelcomeElement
+struct Messages: Codable {
+    let user: User
+    let message: CurrentMessage
+}
+
+// MARK: - Message
+struct CurrentMessage: Codable {
+    let text: String
+    let receivingDate: String
     
-    // MARK: - WelcomeElement
-    struct WelcomeElement: Codable {
-        let user: User
-        let message: Message
+    enum CodingKeys: String, CodingKey {
+        case text
+        case receivingDate = "receiving_date"
     }
+}
 
-    // MARK: - Message
-    struct Message: Codable {
-        let text: String
-        let receivingDate: Date
-
-        enum CodingKeys: String, CodingKey {
-            case text
-            case receivingDate = "receiving_date"
-        }
-    }
-
-    // MARK: - User
-    struct User: Codable {
-        let nickname: String
-        let avatarURL: String
-
-        enum CodingKeys: String, CodingKey {
-            case nickname
-            case avatarURL = "avatar_url"
-        }
+// MARK: - User
+struct User: Codable {
+    let nickname: String
+    let avatarURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case nickname
+        case avatarURL = "avatar_url"
     }
 }
--- MessagesScreen/MessagesScreen/MVP/MessagesPresenter.swift
+++ MessagesScreen/MessagesScreen/MVP/MessagesPresenter.swift
@@ -5,8 +5,27 @@
 //  Created by Nikita Entin on 22.07.2021.
 //
 
-import Foundation
+import UIKit
+
+protocol MessagesPresenterDelegate {
+    func getMessages(messages: [Messages])
+}
+
+typealias PresenterDelegate = MessagesPresenterDelegate & UIViewController
 
 final class MessagesPresenter {
     
+    private let apiManager = APIManager()
+    
+    private weak var delegate: PresenterDelegate?
+    
+    func setViewDelegate(delegate: PresenterDelegate) {
+        self.delegate = delegate
+    }
+    
+    func fetchMessages() {
+        apiManager.fetch { [weak self] (messages: [Messages]) in
+            self?.delegate?.getMessages(messages: messages)
+        }
+    }
 }
